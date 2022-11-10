bbc71895894316e4150e4ab1bc4cd5f045724cde
manarmahmoud <saberxina3@gmail.com>
2022-11-10 21:28:28 +0200

    leaderboard js

--- assets/scripts/exam.js
+++ assets/scripts/exam.js
@@ -1,1 +1,69 @@
-// A
+const firstNumber = Math.ceil(Math.random() * 10);
+const secondNumber = Math.ceil(Math.random() * 10);
+const operation = ["+", "-", "*", "%"];
+const randomOperation = Math.floor(Math.random() * (operation.length));
+const operator = operation[randomOperation];
+// let num = "";
+// let count = 0;
+let results;
+switch (operator) {
+    case '+':
+        results = firstNumber + secondNumber;
+        break;
+    case '-':
+        results = firstNumber - secondNumber;
+        break;
+    case '*':
+        results = firstNumber * secondNumber;
+        break;
+    case '%':
+        results = firstNumber % secondNumber;
+        break;
+    default:
+        results = "notValid"
+        break;
+}
+
+console.log(results)
+const Question = document.querySelector("#question");
+Question.innerText = firstNumber + `${operator}` + secondNumber + "=";
+const buttons = document.querySelectorAll("input[type ='button']");
+const result = document.querySelector('#result');
+const Score = document.querySelector("#score");
+
+let num = "";
+let count = 0;
+Score.innerText = count
+buttons.forEach((btn, i) => {
+   
+    btn.addEventListener("click", (e) => {
+        let value = e.target.value;
+        num += value
+        result.value = num;
+        if (e.target.value == "Clear") {
+            result.value = ""
+        }
+        console.log(num)
+        if (e.target.value == "Enter") {
+           
+            
+            if (parseInt(num) ==results) {
+                alert("Right Answer")
+                count+=10;
+                Score.innerHTML =count;
+                
+
+                
+            }
+            window.location.reload();
+            result.value=" "
+            // console.log(result.value)
+            // Score.innerText = score;
+        }
+
+
+    })
+
+
+})
+
--- assets/scripts/leaderboard.js
+++ assets/scripts/leaderboard.js
@@ -1,7 +1,6 @@
 const getLeaderboardDataFromLocalStorage = () => {
     // some static data, we will replace it with the real data soon
-    const localStorageData = [
-        {
+    const localStorageData = [{
             username: "Aya",
             score: 190
         },
@@ -17,13 +16,40 @@ const getLeaderboardDataFromLocalStorage = () => {
 
     return localStorageData
 }
+const hidden = document.querySelector(".rows-container")
+let cartoona = ""
 
+function refresh(data) {
+    for (let i = 0; i < data.length; i++) {
+        cartoona += `   <div class="exam-content">
+     <div class="text" id="ranks">
+         <p>${i}</p>
+     </div>
+     <div class="text" id="names">
+         <p>${data[i].username}</p>
+     </div>
+     <div class=" text" id="scores">
+         <p>${data[0].score}</p>
+     </div>
+ </div>`
+    }
+    hidden.innerHTML = cartoona;
+}
+
+console.log(hidden)
+const btn_refresh = document.querySelector(".btn-primary")
+console.log(btn_refresh)
 const buildLeaderboard = (data) => {
     console.log(data)
-
     // step 1: empty the div with 'rows-container' class using innerHTML property
+    btn_refresh.addEventListener("click", () => {
+        hidden.innerHTML = "";
+        refresh(data)
+    })
+
 
     // step 2: use a for loop to create a row for each value in the data array
+
 }
 
 // we need to run this function when the file loads and when the refresh button is clicked
